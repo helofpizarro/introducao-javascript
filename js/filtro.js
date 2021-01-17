0068dfaed4648280fe63c93553ffc26410a439fe
Maria Heloisa <heloisa.fpizarro@gmail.com>
2021-01-16 21:44:15 -0300

    filtrando os pacientes da tabela

--- js/filtro.js
+++ js/filtro.js
@@ -1,13 +1,17 @@
-var campoFiltro = document.querySelector('#filtro-tabela')
+var campoFiltro = document.querySelector('#filtrar-tabela')
 
 campoFiltro.addEventListener('input',function () {
     console.log(this.value)
-
-    var paciente = document.querySelectorAll('.paciente')
-    for (var i =0; i < paciente.length; i++){
+    var pacientes = document.querySelectorAll('.paciente')
+    for (var i =0; i < pacientes.length; i++){
         var paciente = pacientes[i]
         var tdNome = paciente.querySelector('.info-nome')
-        var nome = paciente.textContent 
+        var nome = tdNome.textContent
+        if (nome != this.value){
+            paciente.classList.add('invisivel')
+        } else{
+            paciente.classList.remove('invisivel')
+        }
     }
     
 })
